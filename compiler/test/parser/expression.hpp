73c4761be314330bb1c91fa523c0aefe1bffabbd
fatbrother <kevin92422@gmail.com>
2024-01-23 03:46:26 +0800

    feat: add expression structure

--- compiler/test/parser/expression.hpp
+++ compiler/test/parser/expression.hpp
@@ -1,1 +1,37 @@
-// TODO: Add tests for expression
+#include <gtest/gtest.h>
+
+#include <tao/pegtl.hpp>
+
+#include "grammer.hpp"
+
+using namespace tao::pegtl;
+using namespace ddlbx::grammer;
+
+TEST(grammer, expression) {
+    std::string sample1 = "1 = 1";
+    std::string sample2 = "1 =1";
+    std::string sample3 = "1= 1";
+    std::string sample4 = "1=1";
+    std::string sample5 = "1 = 1 = 1";
+    std::string sample6 = "a + b = 1";
+    std::string sample7 = "a";
+    std::string sample8 = "";
+
+    string_input<> in1(sample1, "input");
+    string_input<> in2(sample2, "input");
+    string_input<> in3(sample3, "input");
+    string_input<> in4(sample4, "input");
+    string_input<> in5(sample5, "input");
+    string_input<> in6(sample6, "input");
+    string_input<> in7(sample7, "input");
+    string_input<> in8(sample8, "input");
+
+    EXPECT_TRUE(parse<RightExpression>(in1));
+    EXPECT_TRUE(parse<RightExpression>(in2));
+    EXPECT_TRUE(parse<RightExpression>(in3));
+    EXPECT_TRUE(parse<RightExpression>(in4));
+    EXPECT_TRUE(parse<RightExpression>(in5));
+    EXPECT_TRUE(parse<RightExpression>(in6));
+    EXPECT_TRUE(parse<RightExpression>(in7));
+    EXPECT_FALSE(parse<RightExpression>(in8));
+}
--- compiler/test/parser/right_expression.hpp
+++ compiler/test/parser/right_expression.hpp
@@ -1,1 +1,40 @@
-// TODO: Add tests for right_expression
+#include <gtest/gtest.h>
+
+#include <tao/pegtl.hpp>
+
+#include "grammer.hpp"
+
+using namespace tao::pegtl;
+using namespace ddlbx::grammer;
+
+TEST(grammer, right_expression) {
+    std::string sample1 = "= 1";
+    std::string sample2 = " = 1";
+    std::string sample3 = " =1";
+    std::string sample4 = "=1";
+    std::string sample5 = "=1 = 1";
+    std::string sample6 = "=1 =1";
+    std::string sample7 = "=1= 1";
+    std::string sample8 = "=1=1";
+    std::string sample9 = "1";
+
+    string_input<> in1(sample1, "input");
+    string_input<> in2(sample2, "input");
+    string_input<> in3(sample3, "input");
+    string_input<> in4(sample4, "input");
+    string_input<> in5(sample5, "input");
+    string_input<> in6(sample6, "input");
+    string_input<> in7(sample7, "input");
+    string_input<> in8(sample8, "input");
+    string_input<> in9(sample9, "input");
+
+    EXPECT_TRUE(parse<RightExpression>(in1));
+    EXPECT_TRUE(parse<RightExpression>(in2));
+    EXPECT_TRUE(parse<RightExpression>(in3));
+    EXPECT_TRUE(parse<RightExpression>(in4));
+    EXPECT_TRUE(parse<RightExpression>(in5));
+    EXPECT_TRUE(parse<RightExpression>(in6));
+    EXPECT_TRUE(parse<RightExpression>(in7));
+    EXPECT_TRUE(parse<RightExpression>(in8));
+    EXPECT_FALSE(parse<RightExpression>(in9));
+}
