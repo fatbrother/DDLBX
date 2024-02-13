0a6005fbb432264cff8b3a35c022a38513327093
fatbrother <kevin92422@gmail.com>
2024-02-14 03:44:32 +0800

    feat: add file input functionality to ddlbx.cc

--- compiler/ddlbx.cc
+++ compiler/ddlbx.cc
@@ -1,4 +1,6 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <tao/pegtl.hpp>
 #include <tao/pegtl/contrib/parse_tree.hpp>
 
@@ -7,16 +9,24 @@
 #include "parser/selector.hpp"
 #include "pass/object_genner.hpp"
 
-std::string test = R"(
-fun test(x: Int): Int {
-    ret (1+2+x)*(x+(1+2))!
-}
-)";
-
 namespace pegtl = tao::pegtl;
 
 int main(int argc, char** argv) {
-    pegtl::string_input<> in(test, "input");
+    if (argc != 2) {
+        std::cout << "Usage: ddlbx <file>" << std::endl;
+        return 1;
+    }
+
+    std::string fileNmae(argv[1]);
+    std::ifstream file(fileNmae, std::ios::in);
+    if (!file) {
+        std::cout << "file not found" << std::endl;
+        return 1;
+    }
+
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    pegtl::string_input<> in(content, "input");
     const auto root = pegtl::parse_tree::parse<ddlbx::parser::Program, ddlbx::parser::Selector>(in);
     if (!root) {
         std::cout << "parse error" << std::endl;
@@ -24,14 +34,14 @@ int main(int argc, char** argv) {
     }
 
     llvm::LLVMContext context;
-    llvm::Module module("test", context);
+    llvm::Module module(fileNmae, context);
     ddlbx::ir::CodeGenner codeGenner(context, module);
     codeGenner.generate(root->children[0]);
 
     // print module
     codeGenner.getModule().print(llvm::errs(), nullptr);
 
-    // generate object file
-    ddlbx::pass::ObjectGenner objectGenner;
-    objectGenner.generate(codeGenner.getModule());
+    // // generate object file
+    // ddlbx::pass::ObjectGenner objectGenner;
+    // objectGenner.generate(codeGenner.getModule());
 }
