873950c054b5336847c1f2ba1deaaaaa62578c04
YangCEN311 <ray10609@gmail.com>
2024-03-09 22:16:44 +0800

    fix: Function class

--- compiler/include/ir/function_handler.hpp
+++ compiler/include/ir/function_handler.hpp
@@ -16,10 +16,10 @@ namespace pegtl = tao::pegtl;
 namespace ddlbx{
 namespace ir{
 
-class FunctionHandler{
+class Function{
 public:
 
-    FunctionHandler()
+    Function()
         : function(nullptr) {}
 
     /**
@@ -30,19 +30,64 @@ public:
     void createFunction(llvm::FunctionType* funcType, std::string name, llvm::Module &module){
         function = llvm::Function::Create(funcType, llvm::Function::ExternalLinkage, name, &module);
     }
+    
     /**
      * @brief Get the LLVM Function.
      * 
      * @return llvm::Function& The LLVM Function.
      */
     llvm::Function *getFunction() { return function; }
+
+
+    /**
+     * @brief Insert a new variable alloca into the variable alloca map.
+     * 
+     * @param varibleName The name of the variable.
+     * @param alloca The alloca of the variable.
+    */
+    void insertVaribleAlloca(std::string varibleName, llvm::AllocaInst* alloca){
+        VaribleAlloca[varibleName] = alloca;
+    }
+
+    /**
+     * @brief Get the alloca of a variable.
+     * 
+     * @param varibleName The name of the variable.
+     * 
+     * @return llvm::AllocaInst* The alloca of the variable.
+    */
+    llvm::AllocaInst* getVaribleAlloca(std::string varibleName){
+        return VaribleAlloca[varibleName];
+    }
+
+    /**
+     * @brief Delete a alloca from the variable map.
+     * 
+     * @param varibleName The name of the variable.
+     * 
+     * @return bool True if the variable was deleted, false otherwise.
+    */
+    bool deleteVaribleAlloca(std::string varibleName){
+        if(VaribleAlloca.find(varibleName) != VaribleAlloca.end()){
+            VaribleAlloca.erase(varibleName);
+            return true;
+        }
+        return false;
+    }
+
 private:
     llvm::Function *function;
 
+    std::map<std::string, llvm::AllocaInst*> VaribleAlloca;
+
+    //not sure if this is the way to store the varibles
     std::map<std::string, int> intVarible;
     std::map<std::string, float> floatVarible;
     std::map<std::string, std::string> stringVarible;
     std::map<std::string, bool> boolVarible;
+    
+    
+
 
 };
 }
