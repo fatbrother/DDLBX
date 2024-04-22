649cb08afc5178cdb4894b905db31939111e60d5
fatbrother <kevin92422@gmail.com>
2024-04-23 02:13:27 +0800

    feat: optimize module in ddlbx.cc

--- compiler/ddlbx.cc
+++ compiler/ddlbx.cc
@@ -3,6 +3,13 @@
 #include <string>
 #include <tao/pegtl.hpp>
 #include <tao/pegtl/contrib/parse_tree.hpp>
+#include <tao/pegtl/contrib/parse_tree_to_dot.hpp>
+
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Transforms/Utils.h>
+#include <llvm/Transforms/InstCombine/InstCombine.h>
+#include <llvm/Transforms/Scalar.h>
+#include <llvm/Transforms/Scalar/GVN.h>
 
 #include "ir/code_genner.hpp"
 #include "parser/grammar.hpp"
@@ -26,17 +33,29 @@ int main(int argc, char** argv) {
 
     std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 
-    pegtl::string_input<> in(content, "input");
+    pegtl::string_input<> in(content, "input"); 
     const auto root = pegtl::parse_tree::parse<ddlbx::parser::Program, ddlbx::parser::Selector>(in);
     if (!root) {
         std::cout << "parse error" << std::endl;
         return 0;
     }
 
+    // print parse tree
+    // pegtl::parse_tree::print_dot(std::cout, *root);
+
     llvm::LLVMContext context;
     llvm::Module module(fileNmae, context);
     ddlbx::ir::CodeGenner codeGenner(context, module);
     codeGenner.generate(root->children[0]);
+
+    // optimize module
+    llvm::legacy::PassManager passManager;
+    passManager.add(llvm::createPromoteMemoryToRegisterPass());
+    passManager.add(llvm::createInstructionCombiningPass());
+    passManager.add(llvm::createReassociatePass());
+    passManager.add(llvm::createGVNPass());
+    passManager.add(llvm::createCFGSimplificationPass());
+    passManager.run(module);
 
     // print module
     codeGenner.getModule().print(llvm::errs(), nullptr);
