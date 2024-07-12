974f534d78b759e21d20bff72ee93f82f6a65f54
fatbrother <kevin92422@gmail.com>
2024-07-13 01:43:12 +0800

    chore: Update compiler build script

--- compiler/build.sh
+++ compiler/build.sh
@@ -1,5 +1,33 @@
 root_path=$(cd `dirname $0`; pwd)
 
+create_parser() {
+    echo "Creating parser..."
+
+    mkdir -p src/parser
+    mkdir -p include/parser
+
+    bison -o src/parser/parser.cc --header=include/parser/parser.hpp parser.y -Wcounterexamples
+    lex -o src/parser/token.cc token.l
+
+    echo "Parser created"
+}
+
+DEBUG=false
+
+while getopts "pd" opt; do
+    case $opt in
+        p)
+            create_parser
+            ;;
+        d)
+            DEBUG=true
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;
+    esac
+done
+
 mkdir -p $root_path/build
 mkdir -p $root_path/build/bin
 
