88307ad3638352a2d28e020148ca0864fec5f753
fatbrother <kevin92422@gmail.com>
2024-10-26 10:46:09 +0800

    test: Add test case for simple statement in integration test

--- compiler/test/integration_test.py
+++ compiler/test/integration_test.py
@@ -2,10 +2,11 @@ import os
 import subprocess
 import unittest
 
-class TestDDLBXIntegration(unittest.TestCase):
+class TestCompileExamples(unittest.TestCase):
     EXAMPLES_DIR = "../docs/examples/"
     OUTPUT_DIR = "./output/"
     DDLBX_SCRIPT = "./ddlbx.sh"
+    TEST_CASES_DIR = "./test/cases/"
 
     @classmethod
     def setUpClass(cls):
@@ -16,8 +17,6 @@ class TestDDLBXIntegration(unittest.TestCase):
                 os.remove(os.path.join(cls.OUTPUT_DIR, file))
 
     def test_compile_examples(self):
-        # for each example file in the examples directory
-        # ./ddlbx.sh -i {example_file} -o ./output/{example_file}.out
         is_test_passed = True
         for example_file in os.listdir(self.EXAMPLES_DIR):
             example_file_path = os.path.join(self.EXAMPLES_DIR, example_file)
@@ -26,12 +25,54 @@ class TestDDLBXIntegration(unittest.TestCase):
             print(f"Run Cmd: {self.DDLBX_SCRIPT} -i {example_file_path} -o {output_file_path}")
             subprocess.run([self.DDLBX_SCRIPT, "-i", example_file_path, "-o", output_file_path])
 
-            # check if the output file exists
             if not os.path.exists(output_file_path):
                 is_test_passed = False
                 print(f"Output file {output_file_path} does not exist")
 
         self.assertTrue(is_test_passed)
+
+        is_test_passed = True
+        for example_file in os.listdir(self.EXAMPLES_DIR):
+            example_file_path = os.path.join(self.EXAMPLES_DIR, example_file)
+            output_file_path = os.path.join(self.OUTPUT_DIR, example_file.replace(".ddlbx", ".out"))
+
+            print(f"Run Cmd: {output_file_path}")
+            try:
+                subprocess.run([output_file_path])
+            except subprocess.CalledProcessError as e:
+                is_test_passed = False
+                print(f"Output file {output_file_path} failed to run")
+                print(e)
+
+        self.assertTrue(is_test_passed)
+
+    def test_compile_and_check_output(self):
+        test_cases = {
+            "test1.ddlbx": {
+                "stdin": "",
+                "stdout": "Hello, World!",
+            }
+        }
+
+        for test_file in test_cases.keys():
+            test_file_path = os.path.join(self.TEST_CASES_DIR, test_file)
+            output_file_path = os.path.join(self.OUTPUT_DIR, test_file.replace(".ddlbx", ".out"))
+
+            print(f"Run Cmd: {self.DDLBX_SCRIPT} -i {test_file_path} -o {output_file_path}")
+            subprocess.run([self.DDLBX_SCRIPT, "-i", test_file_path, "-o", output_file_path])
+
+            if not os.path.exists(output_file_path):
+                print(f"Output file {output_file_path} does not exist")
+                self.assertTrue(False)
+
+            print(f"Run Cmd: {output_file_path}")
+            try:
+                process = subprocess.run([output_file_path], input=test_cases[test_file]["stdin"], text=True, capture_output=True)
+                self.assertEqual(process.stdout.strip(), test_cases[test_file]["stdout"])
+            except subprocess.CalledProcessError as e:
+                print(f"Output file {output_file_path} failed to run")
+                print(e)
+                self.assertTrue(False)
 
 
 if __name__ == "__main__":
